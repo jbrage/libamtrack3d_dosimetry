6238834aa272e04a8487f6faa25a38b322fced1c
jbrage <jeppebrage@gmail.com>
2022-03-07 08:38:03 +0100

    test plot

--- track_structure_models.py
+++ track_structure_models.py
@@ -3,41 +3,64 @@ import pyamtrack.libAT as libam
 import numpy as np
 import matplotlib.pyplot as plt
 
-# malloc issue
-# name: list = ['']
-# suc = libam.AT_RDD_name_from_number(1, name)
 
-
-
-# material_name = 'Water, Liquid'
+# define the parameters as in 
+# "Greilich, S., Grzanka, L., Bassler, N., Andersen, C. E., & Jäkel, O. (2010). Amorphous track models: a numerical comparison study. Rad Meas, 45(10), 1406-1409."
 material_name = 'Aluminum Oxide'
+E_MeV_u = 10
+particle_no = libam.AT_particle_no_from_Z_and_A_single(6, 12)
+
+"""
+Problems:
+* I would like to get the core/site radius a0 for each of the RDD, but encounter an error with
+	libam.AT_RDD_a0_m() 
+  "module 'pyamtrack.libAT' has no attribute 'AT_RDD_a0_m'"
+
+  so how I get a0 for a given RDD model?
+  
+* How do I determine which ER model is suitable for the given RDD?
+
+"""
+
+# these should be corrected:
+a0_m = 1e-9
+er_model = 6
+
+# get the material number
 material_no = libam.AT_material_number_from_name(material_name)
 
-er_model = 6
+# get the ER name
 er_model_name = libam.AT_ERModels(er_model)
 
-particle_no = libam.AT_particle_no_from_Z_and_A_single(6, 12)
+# get the particle name 
 particle_name = ['']
 libam.AT_particle_name_from_particle_no_single(particle_no, particle_name)
 
-E_MeV_u = 10
-
-# fix these
-a0 = 1
-rdd_parameter = [a0 * 1e-9, 0, 0]
-
+# get the stopping power
 stopping_power_source_no = libam.stoppingPowerSource_no["PSTAR"].value
 
-RDD_models = [s.name for s in libam.RDDModels]
+# list of RDD models
+# RDD_models = [s.name for s in libam.RDDModels]
 
+# prepare plot
 fig, ax = plt.subplots()
-r_m = list(10**np.linspace(np.log10(1e-11), np.log(1e-3), 100))
-
+r_m = list(10**np.linspace(np.log10(1e-10), np.log(1e-1), 1000))
 D_RDD_Gy = len(r_m)*[0]
 
-for RDD_name in RDD_models:
+# plot each RDD
+for RDD_model in libam.RDDModels:
     
-    rdd_model_no = libam.RDDModels[RDD_name].value
+    rdd_model_no = RDD_model.value
+    
+    # AttributeError: module 'pyamtrack.libAT' has no attribute 'AT_RDD_a0_m'
+    # test = [0, 0, 0]
+    # print(libam.AT_RDD_a0_m(1e-7, rdd_model_no, test))
+    
+    # create a list of the right length
+    n_rdd_parameters = libam.AT_RDD_number_of_parameters(rdd_model_no)
+    rdd_parameter = [0] * n_rdd_parameters
+    if rdd_parameter:
+        rdd_parameter[0] = a0_m
 
     libam.AT_D_RDD_Gy(r_m,
                       E_MeV_u,
@@ -49,8 +72,7 @@ for RDD_name in RDD_models:
                       stopping_power_source_no,
                       D_RDD_Gy)
 
-    label = "{} MeV/u in {} using {}".format(E_MeV_u, material_name, RDD_name)
-    label = "{}".format(RDD_name)
+    label = "{}".format(RDD_model.name.replace("RDD_", ""))
     ax.plot(r_m, D_RDD_Gy, label=label)
 
 ax.set_ylim(ymin=0.1)
